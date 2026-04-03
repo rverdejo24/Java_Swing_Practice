d14efe44ed6cfaf6166bca5a8728dead24fffd79
Rowell <lr.rverdejo@mmdc.mcl.edu.ph>
2026-04-03 11:35:49 +0800

    Add label and textfield component

--- src/main/java/FormPanel.java
+++ src/main/java/FormPanel.java
@@ -1,10 +1,30 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class FormPanel extends JPanel {
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okBtn;
+
     public FormPanel() {
         Dimension dim = getPreferredSize();
         dim.width = 250;
         setPreferredSize(dim);
+
+        nameLabel = new JLabel("Name");
+        occupationLabel = new JLabel("Occupation");
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
+
+        okBtn = new JButton("OK");
+
+        Border innerBorder = BorderFactory.createTitledBorder("Add Person");
+        Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+
+        setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+
     }
 }
--- src/main/java/Toolbar.java
+++ src/main/java/Toolbar.java
@@ -11,6 +11,8 @@ public class Toolbar extends JPanel implements ActionListener {
     private StringListener textListener;
 
     public Toolbar() {
+        setBorder(BorderFactory.createEtchedBorder());
+
         helloButton = new JButton("Hello");
         helloButton.addActionListener(this);
 
