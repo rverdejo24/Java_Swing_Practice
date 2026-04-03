8d9a0a0a3b8eb3281b8db5b05955fe4000e2336b
Rowell <lr.rverdejo@mmdc.mcl.edu.ph>
2026-04-03 12:31:36 +0800

    Implement a GridBagLayout for FormPanel

--- src/main/java/FormPanel.java
+++ src/main/java/FormPanel.java
@@ -14,8 +14,8 @@ public class FormPanel extends JPanel {
         dim.width = 250;
         setPreferredSize(dim);
 
-        nameLabel = new JLabel("Name");
-        occupationLabel = new JLabel("Occupation");
+        nameLabel = new JLabel("Name:");
+        occupationLabel = new JLabel("Occupation:");
         nameField = new JTextField(10);
         occupationField = new JTextField(10);
 
@@ -26,5 +26,49 @@ public class FormPanel extends JPanel {
 
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+
+
+        // First Row
+        gc.weighty = 0.1;
+        gc.weightx = 1;
+
+        gc.gridy = 0;
+        gc.gridx = 0;
+        gc.fill = GridBagConstraints.NONE;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0,0,0,5);
+        add(nameLabel, gc);
+
+        gc.gridy = 0;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0,0,0,0);
+        add(nameField, gc);
+
+        // Second Row
+        gc.weighty = 0.1;
+        gc.weightx = 1;
+        gc.gridy = 1;
+        gc.gridx = 0;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0,0,0,5);
+        add(occupationLabel, gc);
+
+        gc.gridy = 1;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0,0,0,0);
+        add(occupationField, gc);
+
+        // Third Row
+        gc.weighty = 1;
+        gc.weightx = 1;
+        gc.gridy = 2;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets = new Insets(0,0,0,0);
+        add(okBtn, gc);
     }
 }
